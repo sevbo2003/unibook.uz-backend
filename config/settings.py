e371e4ce1f6000b0141a320e6d0772a02dc4ad9c
sevbo2003 <sevbofx@gmail.com>
2022-06-11 17:24:48 +0500

    Some `initial` settings

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party
+    'rest_framework',
+
+    # local
+    'apps.accounts',
+    'apps.authentication',
+    'apps.forum',
+    'apps.forum.notification',
+    'apps.forum.question',
 ]
 
 MIDDLEWARE = [
@@ -91,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
@@ -102,6 +111,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
