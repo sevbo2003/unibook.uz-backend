70667c1a082420e4e27704586bcba9b9316ac93a
sevbo2003 <sevbofx@gmail.com>
2022-06-12 00:54:04 +0500

    `User` model added

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from apps.accounts.models import User
 
-# Register your models here.
+
+admin.site.register(User)
--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from django.utils.timesince import timesince
 
-# Create your models here.
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+    email = models.EmailField(_("email address"))
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+    reputation = models.PositiveIntegerField(_("reputation"), default=0)
+    last_online = models.DateTimeField(_("last online"), default=timezone.now)
+    bio = models.CharField( _("bio"), max_length = 500, null=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True)
+
+    def __str__(self) -> str:
+        return self.username
+    
+    @property
+    def last_activity(self):
+        period = timesince(self.last_online).replace('\xa0', ' ')
+        if period.split()[1] == 'minutes' and int(period.split()[0]) <= 10:
+            return 'online'
+        return period + ' ago'
+    
+    @property
+    def joined(self):
+        if self.date_joined.year == timezone.now().year:
+            return self.date_joined.strftime('%d %b')
+        return self.date_joined.strftime('%d %b, %Y')
+    
+    class Meta:
+        ordering = ["-date_joined"]
+
