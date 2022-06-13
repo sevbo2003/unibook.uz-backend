56711444495b0241ff46929704bb05a15aeac123
sevbo2003 <sevbofx@gmail.com>
2022-06-14 00:22:09 +0500

    Custom `pagination` added

--- apps/accounts/api.py
+++ apps/accounts/api.py
@@ -1,9 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 from apps.accounts.serializers import UserSerializer
 from apps.accounts.models import User
+from apps.accounts.pagination import CustomPagination
 
 
 class UserViewSet(ModelViewSet):
-    queryset = User.objects.all()
+    queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     http_method_names = ['get', 'head', 'options']
+    pagination_class = CustomPagination
