9ad6eb90199afa0c9b42de93f45377ecde87aeb8
Abdusamad <sevbofx@gmail.com>
2022-07-27 15:48:22 +0500

    `extra` changes

--- config/routers.py
+++ config/routers.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from apps.accounts.api import UserViewSet
+from apps.forum.question.api import MainCategoryViewSet, SubCategoryViewSet, QuestionViewSet, CommentViewSet
 
 router = DefaultRouter()
 router.register("users", UserViewSet)
+router.register("main-categories", MainCategoryViewSet)
+router.register("sub-categories", SubCategoryViewSet)
+router.register("questions", QuestionViewSet)
+router.register("comments", CommentViewSet)
 
 
 urlpatterns = [
--- config/urls.py
+++ config/urls.py
@@ -3,14 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt import views as jwt_views
-from apps.authentication.api import RegisterView, LogoutView
+from apps.authentication.api import RegisterView
+from rest_framework import urls as rest_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('config.routers')),
     path('auth/register/', RegisterView.as_view(), name='register'),
-    path("auth/login/", jwt_views.TokenObtainPairView.as_view(), name='login'),
-    path("auth/logout/", LogoutView.as_view(), name='logout'),
+    path("auth/", include(rest_urls), name='login'),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
